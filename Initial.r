b7269d7131625456f103815be936b9f213d9d6a9
hcp4715 <hcp4715@gmail.com>
2019-06-09 15:49:57 +0200

    add plot for exp1a

--- Initial.r
+++ Initial.r
@@ -43,10 +43,10 @@ apatheme = theme_bw()+
               axis.text = element_text (size = 14, color = 'black'),
 #              axis.text.x = element_text(angle = 45, vjust = 0.5),   # x-axis's label font
               axis.title = element_text (size = 14),
-              axis.title.x = element_text(margin=margin(10,0,0,0)),  # increase the sapce betwen title and x axis
-              axis.title.y = element_text(margin=margin(0,12,0,0)),  # increase the space between title and y axis
               axis.line.x = element_line(color='black', size = 1),   # increase the size of font
-              axis.line.y = element_line(color='black', size = 1))   # increase the size of font
+              axis.line.y = element_line(color='black', size = 1),   # increase the size of font
+              axis.title.x = element_text(margin=margin(10,0,0,0)),  # increase the sapce betwen title and x axis
+              axis.title.y = element_text(margin=margin(0,12,0,0)))  # increase the space between title and y axis
 
 
 # define the d prime function
@@ -249,40 +249,74 @@ Mplots <- function(expName = 'exp1', dData,rtData){
               #dData$Identity <- "Valence"
               #rtData$Identity <- "Valence"
               P.dprime <- ggplot(dData,aes(x = Valence, y = dprime, fill = Valence)) +
-                  geom_flat_violin(aes(fill = Valence),position = position_nudge(x = 0.1, y = 0),
-                                   adjust = 1.5, trim = FALSE, alpha = 0.5,color = NA) +
-                  geom_dotplot(aes(x = Valence,y = dprime, color = Valence), 
-                               binaxis='y', binwidth = 0.01, stackdir='center', dotsize= 8,
-                               position = position_dodge(0.2)) +
-                  geom_boxplot(aes(x = Valence,  y = dprime,fill = Valence),outlier.shape = NA,
-                               alpha = 0.5, width = 0.1,  color = "black",
-                               position = position_dodge(0.2)) + 
-                  scale_color_brewer(palette = "Dark2") +
-                  scale_fill_brewer(palette = "Dark2") +
+                geom_flat_violin(aes(fill = Valence),position = position_nudge(x = 0.1, y = 0),
+                                 adjust = 1.5, trim = FALSE, alpha = 0.5,color = NA) +
+                geom_dotplot(aes(x = Valence,y = dprime, color = Valence), 
+                             binaxis='y', binwidth = 0.01, stackdir='center', dotsize= 8,
+                             position = position_dodge(0.2)) +
+                geom_boxplot(aes(x = Valence,  y = dprime,fill = Valence),outlier.shape = NA,
+                             alpha = 0.5, width = 0.1,  color = "black",
+                             position = position_dodge(0.2)) + 
+                scale_color_brewer(palette = "Dark2") +
+                scale_fill_brewer(palette = "Dark2") +
                   #xlab("Valence")+
-                  ylab(expression(paste(italic("d"), " prime"))) +
+                ylab(expression(paste(italic("d"), " prime"))) +
                   #scale_x_discrete(breaks = c(1,2,3),labels = c("Good","Neutral","Bad")) +
-                  scale_y_continuous(expand = c(0, 0), limits = c(-2,6)) +
-                  apatheme
+                scale_y_continuous(expand = c(0, 0), limits = c(-2,6)) +
+                theme_bw()+
+                theme(panel.grid.major = element_blank(),
+                      panel.grid.minor = element_blank(),
+                      panel.background = element_blank(),
+                      panel.border = element_blank(),
+                      text=element_text(family='Times'),
+                      legend.title =element_blank(),
+                      legend.text  = element_text(size =12),
+                      #legend.position='top',
+                      plot.title = element_text(lineheight=.8, face="bold", size = 16),
+                      axis.text = element_text (size = 14, color = 'black'),
+                      #              axis.text.x = element_text(angle = 45, vjust = 0.5),   # x-axis's label font
+                      axis.title = element_text (size = 14),
+                      axis.line.x = element_line(color='black', size = 1),   # increase the size of font
+                      axis.line.y = element_line(color='black', size = 1),   # increase the size of font
+                      axis.title.x = element_blank(),
+                      #axis.title.x = element_text(margin=margin(10,0,0,0)),  # increase the sapce betwen title and x axis
+                      axis.title.y = element_text(margin=margin(0,12,0,0)))  # increase the space between title and y axis
                 
               P.rt <- ggplot(rtData,aes(x = Valence , y = RT, fill = Valence))+
-                  geom_flat_violin(aes(fill = Valence),position = position_nudge(x = 0.1, y = 0),
-                                   adjust = 1.5, trim = FALSE, alpha = 0.5,color = NA) +
+                geom_flat_violin(aes(fill = Valence),position = position_nudge(x = 0.1, y = 0),
+                                 adjust = 1.5, trim = FALSE, alpha = 0.5,color = NA) +
                   #geom_point(aes(x = as.numeric(Morality)-0.15,y = RT, color = Identity), 
                   #           position = position_jitter(width = 0.02),size = 1, shape = 20)+
-                  geom_dotplot(aes(x = Valence,y = RT, color = Valence), 
-                               binaxis='y', binwidth = 0.4, stackdir='center', dotsize= 16,position = position_dodge(0.2)) + 
-                  geom_boxplot(aes(x = Valence,  y = RT,fill = Valence),outlier.shape = NA,
-                               alpha = 0.5, width = 0.1,  color = "black",
-                               position = position_dodge(0.2))+ 
-                  scale_color_brewer(palette = "Dark2")+
-                  scale_fill_brewer(palette = "Dark2")+
-                  ylab("Reaction Times")+
+                geom_dotplot(aes(x = Valence,y = RT, color = Valence), 
+                             binaxis='y', binwidth = 0.4, stackdir='center', dotsize= 16,position = position_dodge(0.2)) + 
+                geom_boxplot(aes(x = Valence,  y = RT,fill = Valence),outlier.shape = NA,
+                             alpha = 0.5, width = 0.1,  color = "black",
+                             position = position_dodge(0.2))+ 
+                scale_color_brewer(palette = "Dark2")+
+                scale_fill_brewer(palette = "Dark2")+
+                ylab("Reaction Times")+
                   #scale_x_discrete(breaks = 'Morality',label = 'Morality') +
                   #xlab("Valence") +
-                  scale_y_continuous(expand = c(0, 0),limits = c(200,1000))+
-                  #theme(axis.ticks.x=element_blank())+
-                  apatheme 
+                scale_y_continuous(expand = c(0, 0),limits = c(200,1000))+
+                theme_bw()+
+                theme(panel.grid.major = element_blank(),
+                      panel.grid.minor = element_blank(),
+                      panel.background = element_blank(),
+                      panel.border = element_blank(),
+                      text=element_text(family='Times'),
+                      legend.title =element_blank(),
+                      legend.text  = element_text(size =12),
+                      #legend.position='top',
+                      plot.title = element_text(lineheight=.8, face="bold", size = 16),
+                      axis.text = element_text (size = 14, color = 'black'),
+                      #              axis.text.x = element_text(angle = 45, vjust = 0.5),   # x-axis's label font
+                      axis.title = element_text (size = 14),
+                      axis.line.x = element_line(color='black', size = 1),   # increase the size of font
+                      axis.line.y = element_line(color='black', size = 1),   # increase the size of font
+                      axis.title.x = element_blank(),
+                      #axis.title.x = element_text(margin=margin(10,0,0,0)),  # increase the sapce betwen title and x axis
+                      axis.title.y = element_text(margin=margin(0,12,0,0)))  # increase the space between title and y axis
+              
               # save the d-prime plot
               #fileName = paste0('p_',expName,'_match_dprime','.pdf')
               #ggsave(fileName, P.dprime, scale = 1,height = 6, width = 5, dpi = 300, family = "Times",path = saveDir)
